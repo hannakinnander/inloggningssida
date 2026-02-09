314057714d672a8810b4415057c3f26f853c844e
hannakinnander <kinnander95@live.se>
2026-02-09 16:58:02 +0100

    tried a version of using hidden when clicking login-button

---
+++ script.js
@@ -0,0 +1,7 @@
+const loginField = document.getElementById("login-field");
+const loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener("click", function (){
+loginField.classList.add("hidden");
+})
+
