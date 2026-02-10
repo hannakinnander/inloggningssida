7d6440152cbe8d6d778d7a6f23b7f9c2162aae15
hannakinnander <kinnander95@live.se>
2026-02-10 16:40:18 +0100

    fixed the function to add user and hide elements in different stages

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function goToAddUserPage(){
     loginField.classList.add("hidden");
     addUserField.classList.remove("hidden");
     goBack.classList.remove("hidden");
+    occupiedUsername.classList.add("hidden");
+    document.querySelector(".firstStep").classList.remove("hidden");
+    
     };
     
 newUserButton.addEventListener("click", goToAddUserPage);
@@ -26,48 +29,68 @@ newUserButton.addEventListener("click", goToAddUserPage);
 goBack.addEventListener("click", function (){
     loginField.classList.toggle("hidden");
     addUserField.classList.add("hidden");
-    goBack.classList.add("hidden");
+    goBack.classList.add("hidden")
+    newUsername.value = "";
+    newPassword.value = "";
+    tryNewPassword.value = "";
+    newAccountAdded.classList.add("hidden");
+     document.querySelector(".addPassword").classList.add("hidden");
+    
+    
 })
 //När man trycker "Logga ut"
 
 //När man anger nytt användarnamn
-
-const savedUsers = [];  
+let savedUsers = JSON.parse(localStorage.getItem("user")) || [];  
 const newUsername = document.getElementById("newUsername");
 const newPassword = document.getElementById("newPassword");
 const tryNewPassword = document.getElementById("tryNewPassword");
 const createAccount = document.querySelector(".createAccount");
 const noMatchPassword = document.querySelector(".noMatchPassword");
+const newAccountAdded = document.querySelector(".newAccountAdded");
 
 const occupiedUsername = document.querySelector(".occupiedUsername");
 const tryUsername = document.querySelector(".tryUsername");
+
 tryUsername.addEventListener("click", addNewUser);
 
 function addNewUser(){
+    
     const name = newUsername.value;
-    if (savedUsers.includes(name)){
-        occupiedUsername.textContent = "Användarnamnet är upptaget"
-        
+    const usernameTaken = savedUsers.some(
+        newAccount => newAccount.username === name);
+    
+    if (usernameTaken){
+        occupiedUsername.classList.remove("hidden");
+        return;
     }
     else {
+        
         document.querySelector(".firstStep").classList.add("hidden");
         document.querySelector(".addPassword").classList.remove("hidden");
         createAccount.addEventListener("click", function(){
             if (newPassword.value === tryNewPassword.value){
                 const newAccount = {
-                    name: newUsername.value,
+                    username: name,
                     password: newPassword.value
                 };
                 savedUsers.push(newAccount);
-                localStorage.setItem("users", JSON.stringify(savedUsers));
+                localStorage.setItem("user", JSON.stringify(savedUsers));
+                newAccountAdded.classList.remove("hidden");
+                newAccountAdded.textContent= "Användaren " + name + " har lagts till!"
+                document.querySelector(".addPassword").classList.add("hidden");
+
+
             }
             else{
                 noMatchPassword.classList.remove("hidden");
-                noMatchPassword.textContent = "Lösenorden matchar inte";
+                
             }
         })
         
     }
 };
+
+
 
 
