ba7ce9a2b9e790b831daeb56053c75ab60701709
hannakinnander <kinnander95@live.se>
2026-02-10 10:26:48 +0100

    added function to the homepage-button so that the right header-buttons disappeard and the login-section showed again

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const newUserButton = document.getElementById("newUser");
 const addUserField = document.querySelector(".addUser")
+const headerRight = document.querySelector(".headerRight");
+
 const loginField = document.getElementById("login-field");
 const loginButton = document.getElementById("loginButton");
 const usernameInput = document.getElementById("username");
@@ -8,8 +10,15 @@ const passwordInput = document.getElementById("password");
 function goToAddUserPage(){
     loginField.classList.add("hidden");
     addUserField.hidden = false;
-    document.querySelector("headerRight").hidden = false;
+    headerRight.hidden = false;
 
 }
 
 newUserButton.addEventListener("click", goToAddUserPage);
+
+const toHomePage = document.getElementById("goBack");
+toHomePage.addEventListener("click", function (){
+    loginField.classList.toggle("hidden");
+    addUserField.hidden = true;
+    headerRight.hidden = true;
+})
