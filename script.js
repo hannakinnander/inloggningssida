6d5c28a61dce9e143f1d476f7f211f4e5d993a2d
hannakinnander <kinnander95@live.se>
2026-02-10 14:21:00 +0100

    started to experiment with saving a new account.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Element som har med "lägg till ny användare" att göra.
 const newUserButton = document.getElementById("newUser");
-const addUserField = document.getElementById("addUser");
-const headerRight = document.querySelector(".headerRight");
+const addUserField = document.getElementById("addUsername");
+const goBack = document.querySelector(".goBack");
+
 
 //Element som har med inloggnings-sektionen att göra.
 const loginField = document.getElementById("login-field");
@@ -11,19 +12,62 @@ const passwordInput = document.getElementById("password");
 
 addUserField.classList.add("hidden");
 
+//När man trycker "Registrera konto"
 function goToAddUserPage(){
     loginField.classList.add("hidden");
     addUserField.classList.remove("hidden");
-    headerRight.hidden = false;
+    goBack.classList.remove("hidden");
     };
     
-
-
 newUserButton.addEventListener("click", goToAddUserPage);
 
-const toHomePage = document.getElementById("goBack");
-toHomePage.addEventListener("click", function (){
+//När man trycker "Tillbaka till startsida"
+
+goBack.addEventListener("click", function (){
     loginField.classList.toggle("hidden");
     addUserField.classList.add("hidden");
-    headerRight.hidden = true;
+    goBack.classList.add("hidden");
 })
+//När man trycker "Logga ut"
+
+//När man anger nytt användarnamn
+
+const savedUsers = [];  
+const newUsername = document.getElementById("newUsername");
+const newPassword = document.getElementById("newPassword");
+const tryNewPassword = document.getElementById("tryNewPassword");
+const createAccount = document.querySelector(".createAccount");
+const noMatchPassword = document.querySelector(".noMatchPassword");
+
+const occupiedUsername = document.querySelector(".occupiedUsername");
+const tryUsername = document.querySelector(".tryUsername");
+tryUsername.addEventListener("click", addNewUser);
+
+function addNewUser(){
+    const name = newUsername.value;
+    if (savedUsers.includes(name)){
+        occupiedUsername.textContent = "Användarnamnet är upptaget"
+        
+    }
+    else {
+        document.querySelector(".firstStep").classList.add("hidden");
+        document.querySelector(".addPassword").classList.remove("hidden");
+        createAccount.addEventListener("click", function(){
+            if (newPassword.value === tryNewPassword.value){
+                const newAccount = {
+                    name: newUsername.value,
+                    password: newPassword.value
+                };
+                savedUsers.push(newAccount);
+                localStorage.setItem("users", JSON.stringify(savedUsers));
+            }
+            else{
+                noMatchPassword.classList.remove("hidden");
+                noMatchPassword.textContent = "Lösenorden matchar inte";
+            }
+        })
+        
+    }
+};
+
+
