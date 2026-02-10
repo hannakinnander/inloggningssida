b2c9d20830bb9739535aad6564678d8528a1236f
hannakinnander <kinnander95@live.se>
2026-02-10 11:29:12 +0100

    fixed some problems with the hidden-functions on 'addUser'

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
+// Element som har med "lägg till ny användare" att göra.
 const newUserButton = document.getElementById("newUser");
-const addUserField = document.querySelector(".addUser")
+const addUserField = document.getElementById("addUser");
 const headerRight = document.querySelector(".headerRight");
 
+//Element som har med inloggnings-sektionen att göra.
 const loginField = document.getElementById("login-field");
 const loginButton = document.getElementById("loginButton");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 
+addUserField.classList.add("hidden");
+
 function goToAddUserPage(){
     loginField.classList.add("hidden");
-    addUserField.hidden = false;
+    addUserField.classList.remove("hidden");
     headerRight.hidden = false;
+    };
+    
 
-}
 
 newUserButton.addEventListener("click", goToAddUserPage);
 
 const toHomePage = document.getElementById("goBack");
 toHomePage.addEventListener("click", function (){
     loginField.classList.toggle("hidden");
-    addUserField.hidden = true;
+    addUserField.classList.add("hidden");
     headerRight.hidden = true;
 })
