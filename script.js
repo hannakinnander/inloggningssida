b37df10bb3bea484786ec63e2ec44bf10d52bc11
hannakinnander <kinnander95@live.se>
2026-02-11 16:20:53 +0100

    changed all id's to classes and started over with the js-file. Fixed log-in and started to work on addNewUserField

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-function updateStatus(){
-let loggedInUser = localStorage.getItem("loggedIn");
-if (loggedInUser){
-    loggedIn.classList.remove("hidden");
-    welcomeMessage.classList.remove("hidden");
-    welcomeMessage.textContent = "Inloggad som " + loggedInUser;
-    loginField.classList.add("hidden");
-    changeAccountButton.classList.remove("hidden");
-    newAccountAdded.classList.add("hidden");
-    logOut.classList.remove("hidden");
-}
-else {
-    loggedIn.classList.add("hidden");
-    loginField.classList.remove("hidden");
-    changeAccountButton.classList.add("hidden");
-    logOut.classList.add("hidden");
-    goBack.classList.add("hidden");
-    loginButton.textContent = "Logga in";
-}
-}//Kolla sparat minne när sidan laddas
-updateStatus();
-let savedUsers = JSON.parse(localStorage.getItem("user")) || [];  
-let username = null;
 //Headerns element
 const goBack = document.querySelector(".goBack");
 const changeAccountButton = document.querySelector(".changeAccount");
 const logOut = document.querySelector(".logOut");
 
 //Element som har med inloggnings-sektionen att göra.
-const loginField = document.getElementById("login-field");
-const loginButton = document.getElementById("loginButton");
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-const newUserButton = document.getElementById("newUser");
+const loginField = document.querySelector(".loginField")
+const usernameInput = document.querySelector(".username");
+const passwordInput = document.querySelector(".password");
+const cantLogin = document.querySelector(".cantLogin");
+const loginButton = document.querySelector(".loginButton");
+
+const newUserButton = document.querySelector(".newUser");
 
 //Element som har med skapa ny användare att göra
-const addUserField = document.getElementById("addUsername");
+const addNewUserField = document.querySelector(".addNewUserField");
 const checkUsernameBox = document.querySelector(".checkUsernameBox");
-const newUsername = document.getElementById("newUsername");
+const newUsername = document.querySelector(".newUsername");
 const occupiedUsername = document.querySelector(".occupiedUsername");
 const tryUsername = document.querySelector(".tryUsername");
-const newPassword = document.getElementById("newPassword");
-const tryNewPassword = document.getElementById("tryNewPassword");
+const newPassword = document.querySelector(".newPassword");
+const tryNewPassword = document.querySelector(".tryNewPassword");
 const createAccount = document.querySelector(".createAccount");
 const noMatchPassword = document.querySelector(".noMatchPassword");
 const newAccountAdded = document.querySelector(".newAccountAdded");
@@ -49,86 +28,18 @@ const newAccountAdded = document.querySelector(".newAccountAdded");
 const loggedIn= document.querySelector(".loggedIn");
 const welcomeMessage = document.querySelector(".loggedIn h3");
 
-changeAccountButton.addEventListener("click", goToLogin);
-
-function goToLogin(){
-    loginField.classList.remove("hidden");
-    loginButton.textContent = "Byt användare";
-}
-
-
-//När man trycker "Registrera konto"
-function goToAddUserPage(){
-    loginField.classList.add("hidden");
-    addUserField.classList.remove("hidden");
-    checkUsernameBox.classList.remove("hidden");
-    goBack.classList.remove("hidden");
-    occupiedUsername.classList.add("hidden");
-    };
-    
-newUserButton.addEventListener("click", goToAddUserPage);
-
-//När man trycker "Tillbaka till startsida"
-
-goBack.addEventListener("click", function (){
-    
-    newUsername.value = "";
-    newPassword.value = "";
-    tryNewPassword.value = "";
-    usernameInput.value = "";
-    passwordInput.value= "";
-    updateStatus();
-    
-})
-//När man trycker "Logga ut"
-
-//När man anger nytt användarnamn
-tryUsername.addEventListener("click", addNewUser);
-
-function addNewUser(){
-    username = newUsername.value.trim();
-    const usernameTaken = savedUsers.some(
-        newAccount => newAccount.username === username);
-    
-    if (usernameTaken){
-        occupiedUsername.classList.remove("hidden");
-        return;
-    }
-    else {
-        document.querySelector(".firstStep").classList.add("hidden");
-        document.querySelector(".addPassword").classList.remove("hidden");
-    }
-}
-createAccount.addEventListener("click", createAccountHandler);
-
-function createAccountHandler(){
-    if (newPassword.value === tryNewPassword.value){
-        const newAccount = {
-            username: username,
-            password: newPassword.value
-            };
-        savedUsers.push(newAccount);
-        localStorage.setItem("user", JSON.stringify(savedUsers));
-        newAccountAdded.classList.remove("hidden");
-        newAccountAdded.textContent= "Användaren " + username + " har lagts till!"
-        document.querySelector(".addPassword").classList.add("hidden");
-    }
-    else{
-        noMatchPassword.classList.remove("hidden");
-        return;
-    }
-};
-   //När man trycker logga in
-    loginButton.addEventListener("click", logIn);
+//Behållare för användare
+let savedUsers = [];
+let username = null;
+//Logga in
+ loginButton.addEventListener("click", logIn);
 
     function logIn(){
        savedUsers = JSON.parse(localStorage.getItem("user")) || [];
        username = usernameInput.value.trim();
        const password = passwordInput.value;
-       
        const foundUser = savedUsers.find(user => user.username === username &&
-            user.password === password 
-       );
+            user.password === password);
        if (foundUser){
             loginField.classList.add("hidden");
             goBack.classList.remove("hidden");
@@ -138,8 +49,17 @@ function createAccountHandler(){
             localStorage.setItem("loggedIn", username);
        }
        else{
-        document.querySelector(".cantLogin").classList.remove("hidden");
+        cantLogin.classList.remove("hidden");
        }
     }
 
-
+    //När man trycker Registrera dig här
+    newUserButton.addEventListener("click", goToAddUserPage);
+    function goToAddUserPage(){
+    loginField.classList.add("hidden");
+    addNewUserField.classList.remove("hidden");
+    checkUsernameBox.classList.remove("hidden");
+    goBack.classList.remove("hidden");
+    
+    };
+    
